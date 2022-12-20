08d61a39dfcd87e0a5181dd84b72ce966b8c7456
Maya Baznkou <mayamoaaz2021@gmail.com>
2022-12-20 15:20:57 +0300

    Add file

---
+++ Class-07/Maya Baznkou/hoisting.js
@@ -0,0 +1,12 @@
+'use strict'//excute my code in strict mode
+console.log("I am alive")
+//hoisting:
+
+function hoisting(){
+    console.log("I am running, can you see me")
+}
+hoisting()
+
+number = 5;
+console.log(number);
+var number;
